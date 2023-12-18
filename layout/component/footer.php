06da798e9ec6c997697077b74f92bd7398b05fe1
Diwa Perkasa <diwa.perkasa@gmail.com>
2023-12-18 11:55:51 +0700

    fix footer

--- layout/component/footer.php
+++ layout/component/footer.php
@@ -6,7 +6,7 @@
                     <div>
                         <div class="row align-items-end mb-3">
                             <div class="col-md-3">
-                                <span class="logo fw-normal">R.S.V.P</span>
+                                <span class="logo fw-normal text-light">R.S.V.P</span>
                             </div>
                             <div class="col-md-9">
                                 <h5 class="fw-normal">Don’t miss out. Subscribe to news from R.S.V.P</h5>
@@ -40,15 +40,14 @@
             <div class="col-md-6">
                 <div class="row">
                     <div class="input-group mb-5">
-                        <input type="email" class="form-control bg-transparent border-bottom text-white" placeholder="Email Address" aria-label="Email Address" aria-describedby="button-addon2">
-                        <button class="btn btn-outline-secondary border-bottom text-white" type="button" id="button-addon2">SUBSCRIBE</button>
+                        <input type="email" class="form-control bg-transparent border-bottom text-white px-0" placeholder="Email Address" aria-label="Email Address" aria-describedby="button-addon2">
+                        <button class="btn btn-outline-secondary border-bottom text-white px-0" type="button" id="button-addon2">SUBSCRIBE</button>
                     </div>
                 </div>
                 <div class="row">
                     <div class="col-4">
                         <p class="h4 mb-4 fw-normal">About</p>
                         <ul class="list-unstyled">
-                            <li class="mb-2"><a href="/">Home</a></li>
                             <li class="mb-2"><a href="/about">About</a></li>
                             <li class="mb-2"><a href="/service">Services</a></li>
                             <li class="mb-2"><a href="/projects">Portfolio</a></li>
