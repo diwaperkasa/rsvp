80e2d7106516541517cf6f06ac9b74557bc270bd
Diwa Perkasa <diwa.perkasa@gmail.com>
2023-12-19 08:35:57 +0700

    fix js version

--- layout/page/homepage.php
+++ layout/page/homepage.php
@@ -1,6 +1,6 @@
 <?php
     // add custom script
-    wp_enqueue_script('homepage', get_stylesheet_directory_uri() . '/assets/js/homepage.js', [], false, true);
+    wp_enqueue_script('homepage', get_stylesheet_directory_uri() . '/assets/js/homepage.js', [], "1.0", true);
 ?>
 
 <section id="section-hero">
