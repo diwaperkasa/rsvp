67c23305d7fa27696dce0220026e19e349de2b25
Diwa Perkasa <diwa.perkasa@gmail.com>
2023-12-20 13:37:44 +0700

    add logo

--- layout/main_layout.php
+++ layout/main_layout.php
@@ -6,6 +6,11 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= wp_get_document_title() ?></title>
+    <meta name="description" content="We Innovate Your Story">
+    <link rel="apple-touch-icon" sizes="180x180" href="<?= get_stylesheet_directory_uri() . "/assets/favicons/apple-touch-icon.png" ?>">
+	<link rel="icon" type="image/png" href="<?= get_stylesheet_directory_uri() . "/assets/favicons/favicon-32x32.png" ?>" sizes="32x32">
+	<link rel="icon" type="image/png" href="<?= get_stylesheet_directory_uri() . "/assets/favicons/favicon-16x16.png" ?>" sizes="16x16">
+	<link rel="mask-icon" href="<?= get_stylesheet_directory_uri() . "/assets/favicons/safari-pinned-tab.svg" ?>" color="#403e40">
     <?php wp_head() ?>
 </head>
 
