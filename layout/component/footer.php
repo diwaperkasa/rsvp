5914d282cbf4b71df9073bc6dd8aef66f2ea7ca3
Diwa Perkasa <diwa.perkasa@gmail.com>
2024-01-29 15:19:50 +0700

    update feedback

--- layout/component/footer.php
+++ layout/component/footer.php
@@ -5,7 +5,7 @@
                 <div class="h-100 d-flex flex-column justify-content-between">
                     <div>
                         <div class="row align-items-end mb-3">
-                            <div class="col-md-3">
+                            <div class="col-md-3 d-none">
                                 <img class="img-logo" height="25px" src="<?= get_stylesheet_directory_uri() . "/assets/img/R.S.V.P Logo.png" ?>" />
                                 <!-- <span class="logo fw-normal text-light">R.S.V.P</span> -->
                             </div>
@@ -13,7 +13,7 @@
                                 <h5 class="fw-normal">Don’t miss out. Subscribe to news from R.S.V.P</h5>
                             </div>
                         </div>
-                        <div class="mb-5 d-none d-sm-block">
+                        <div class="mb-5 d-none">
                             <a href="#" class="h2 me-2">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-facebook" viewBox="0 0 16 16">
                                     <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951" />
