eaa50d5d0f7ea36b79c30578352ecff5b12d671a
diwaperkasa <diwa.perkasa@gmail.com>
2026-01-23 13:50:03 +0700

    update page

--- functions.php
+++ functions.php
@@ -331,7 +331,9 @@ function rsvp_get_services(int $limit = -1)
 {
     $args = [
         'posts_per_page' => $limit,
-        'orderby' => 'ID',
+        // 'orderby' => 'ID',
+        'meta_key'  => 'position',
+        'orderby'   => 'meta_value_num',
         'order' => 'ASC',
         'post_type' => 'post',
         'post_status' => 'publish',
