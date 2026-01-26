d5018053e5846f6a3677c3b9eec67a227b69299c
diwaperkasa <diwa.perkasa@gmail.com>
2026-01-26 19:07:15 +0700

    update code

--- layout/page/projects.php
+++ layout/page/projects.php
@@ -12,46 +12,24 @@
             Curated <span class="fst-italic">Selection</span><br>
             of <span class="fst-italic">Projects</span>
         </h1>
-        <?php foreach ($result->get_posts() as $key => $post): ?>
-            <?php
-                $thumbnail = carbon_get_post_meta($post->ID, 'project_thumbnail');
-            ?>
-            <?php if ($rowIndex == 0): ?>
-                <div class="row align-items-center justify-content-center">
-            <?php endif; ?>
-            <div class="<?= ($groupIndex % 2 == 0) ? 
-                (($postSize - (($groupIndex + 1) * $postPerRow) < $postPerRow) ?
-                    ("col-md-" . (5 - ($postSize - $key)))
-                    : ($rowIndex % 2 == 0 ? "col-md" : "col-md-5"))
-                : ($rowIndex % 2 == 0 ? "col-md" : "col-md-3") ?>">
-                <a href="<?= get_permalink($post) ?>">
-                    <div class="card rounded-0  border-0">
-                        <div class="card-body">
-                            <p class="mb-0 dotdec position-relative ms-4"><?= $key + 1 ?></p>
-                            <div class="img-list-container">
-                                <img src="<?= wp_get_attachment_url($thumbnail ? $thumbnail[0] : 0) ?>" alt="...">
+        <div class="row align-items-center justify-content-center">
+            <?php foreach ($result->get_posts() as $key => $post): ?>
+                <div class="<?= ($key % 2 == 0) ? "col-md-3" : "col-md-4" ?>">
+                    <a href="<?= get_permalink($post) ?>">
+                        <div class="card rounded-0  border-0">
+                            <div class="card-body">
+                                <p class="mb-0 dotdec position-relative ms-4"><?= $key + 1 ?></p>
+                                <div class="img-list-container">
+                                    <img src="<?= wp_get_attachment_url($thumbnail ? $thumbnail[0] : 0) ?>" alt="...">
+                                </div>
+                                <h5 class="card-title mt-3 mb-0 fw-normal"><?= $post->post_title ?></h5>
+                                <!-- <p class="card-text"><?= carbon_get_post_meta( $post->ID, 'project_year' ) ?></p> -->
                             </div>
-                            <h5 class="card-title mt-3 mb-0 fw-normal"><?= $post->post_title ?></h5>
-                            <!-- <p class="card-text"><?= carbon_get_post_meta( $post->ID, 'project_year' ) ?></p> -->
                         </div>
-                    </div>
-                </a>
-            </div>
-            <?php ++$rowIndex; ?>
-            <?php if ($rowIndex == $postPerRow): ?>
+                    </a>
                 </div>
-            <?php endif; ?>
-            <?php
-                if ($rowIndex == $postPerRow) {
-                    ++$groupIndex;
-                }
-
-                $rowIndex = $rowIndex == 3 ? 0 : $rowIndex;
-            ?>
-        <?php endforeach; ?>
-        <?php if ($rowIndex != $postPerRow): ?>
-            </div>
-        <?php endif; ?>
+            <?php endforeach; ?>
+        </div>
         <div class="text-center mt-5">
             <a class="fw-normal h5 fst-italic text-decoration-underline" href="/services" role="button">Browse All Services</a>
             <span class="ms-2">
