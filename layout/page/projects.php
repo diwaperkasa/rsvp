b71abc7d75740c473bfda13e354054c70ffe86dc
diwaperkasa <diwa.perkasa@gmail.com>
2026-01-26 20:45:25 +0700

    update code

--- layout/page/projects.php
+++ layout/page/projects.php
@@ -15,7 +15,7 @@
         <div class="row align-items-center justify-content-center">
             <?php foreach ($result->get_posts() as $key => $post): ?>
                 <?php $thumbnail = carbon_get_post_meta($post->ID, 'project_thumbnail'); ?>
-                <div class="<?= ($key % 2 == 0) ? "col-md-3" : "col-md-4" ?>">
+                <div class="col-sm-6 <?= ($key % 2 == 0) ? "col-md-3" : "col-md-4" ?>">
                     <a href="<?= get_permalink($post) ?>">
                         <div class="card rounded-0  border-0">
                             <div class="card-body">
